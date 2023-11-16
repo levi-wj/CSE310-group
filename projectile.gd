547f75a2f7660ca58df4460aa358606d7fd7b09b
Levi Jones <leviwill.j@gmail.com>
2023-11-16 09:26:46 -0700

    Fireballs despawn when offscreen

--- projectile.gd
+++ projectile.gd
@@ -1,12 +1,23 @@
 extends Area2D
 
 @export var speed = 100
+@onready var playergroup = get_tree().get_nodes_in_group("Player")
+
+const CUTOFF_DISTANCE = 1000;
+var player;
 
 func _ready():
+	if playergroup:
+		player = playergroup[0]
 	body_entered.connect(_on_EnemyBodyEntered)
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
+	var distance = global_transform.origin.distance_to(player.global_transform.origin)
+	if distance > CUTOFF_DISTANCE:
+		print("delete")
+		queue_free()
+
 
 func _on_Bullet_body_entered(body):
 	if not body.is_in_group("Player"):
