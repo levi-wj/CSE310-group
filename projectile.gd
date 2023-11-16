4bf5e98d17ae09d4f761c7503775d16d40f6a751
Nathan Bird <nathanbirdka@gmail.com>
2023-11-16 09:13:29 -0700

    Enemy fireball collision (#23)
    * Remove ants if hit by fireball
    
    * Clean up scripts

--- projectile.gd
+++ projectile.gd
@@ -2,11 +2,19 @@ extends Area2D
 
 @export var speed = 100
 
+func _ready():
+	body_entered.connect(_on_EnemyBodyEntered)
+
 func _physics_process(delta):
 	position += transform.x * speed * delta
 
 func _on_Bullet_body_entered(body):
-	if not body.is_in_group("player"):
+	if not body.is_in_group("Player"):
 		# If we hit anything except ourselves, delete the projectile
 		queue_free()
 		print("hit something")
+
+func _on_EnemyBodyEntered(body):
+	if body.is_in_group("Enemies"):
+		body.queue_free()
+		queue_free()
