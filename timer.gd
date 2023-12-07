eef274dada07ee9b175d1790d30c75772cc128e3
Nathan Bird <nathanbirdka@gmail.com>
2023-12-07 10:00:14 -0700

    Add timer scene and script to git tracking

---
+++ timer.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var running = false
+
+var time = 0
+var minutes = 0
+var seconds = 0
+
+
+func _process(delta):
+	if running:
+		time += delta
+		seconds = fmod(time, 60)
+		minutes = fmod(time, 3600) / 60
+		
+		$CanvasLayer/Panel/Label.text = "%02d:%02d" % [minutes, seconds]
+
+func start_timer():
+	running = true
+
+func stop_timer():
+	running = false
+
+func get_score():
+	return time
