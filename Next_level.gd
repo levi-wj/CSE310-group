88308a6d15af2084026aac8b95d89bd7b8473397
Levi Jones <leviwill.j@gmail.com>
2023-12-09 16:25:43 -0700

    Added next level to all levels

--- Next_level.gd
+++ Next_level.gd
@@ -13,6 +13,6 @@ func _on_body_entered(body):
 		var current_scene_file = get_tree().current_scene.scene_file_path
 		var next_level_number = current_scene_file.to_int()
 		print(next_level_number)
-		var next_level_path = "res://" + level_names[next_level_number] + ".tscn"
+		var next_level_path = "res://Levels/" + level_names[next_level_number] + ".tscn"
 		get_tree().change_scene_to_file(next_level_path)
 		
