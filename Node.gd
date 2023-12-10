366d2b368fb412c757fbbaad329ebeb3e5c5d492
Levi Jones <leviwill.j@gmail.com>
2023-12-09 17:21:59 -0700

    Highscore scene

--- Node.gd
+++ Node.gd
@@ -1,15 +1,14 @@
 extends Node
 
+@onready var ui_row = preload("res://highscore_row.tscn")
 
 var data_raw = {
 	"collection":"high_score_data",
 	"database":"high_score",
 	"dataSource":"Cluster0",
-	"sort": {"score":-1},
+	"sort": {"score":1},
 	"limit": 10	
 }
-
-
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,13 +17,13 @@ func _ready():
 	var json = JSON.stringify(data_raw)
 	var headers = ["Content-Type: application/json","Access-Control-Request-Headers: *","api-key: VOII2w3SVRVHq8vQ3m9L6he86He45zXRA8efsRYDyWMXhADSNYaNJdH1DRp0hUKs"]
 	var data_request = $HTTPRequest.request("https://us-west-2.aws.data.mongodb-api.com/app/data-hicfs/endpoint/data/v1/action/find", headers, HTTPClient.METHOD_POST,json)
-	print(data_request)
 	
 
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	print(json)
+	var container = $"../MarginContainer/Highscore/VBoxContainer2";
 	
-
-
-	
+	for score in json.documents:
+		var instance = ui_row.instantiate()
+		instance.get_node("Panel").get_node("Label").text = str(score.score);
+		container.add_child(instance)
